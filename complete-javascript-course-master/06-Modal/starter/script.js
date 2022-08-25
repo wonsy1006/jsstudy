69b430d0cf7125b3b300d3de14e74f42cb26f6c6
wonsy1006 <wonsy1006@gmail.com>
2022-08-25 23:56:07 +0900

    2022 / 08 / 25 in the middle of building pig game, still needs to fix most of the codes

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -7,10 +7,29 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsShowModal);
 
+const showModal = i => {
+  console.log(`Button${i + 1} clicked`);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', () => {
-    console.log(`Button${i + 1} clicked`);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsShowModal[i].addEventListener('click', showModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// keyboard events
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,56 @@
 'use strict';
+// Selecting elements
+const players = [
+  document.querySelector('.player--0'),
+  document.querySelector('.player--1'),
+];
+
+const scoreEls = [
+  document.querySelector('#score--0'),
+  document.querySelector('#score--1'),
+];
+
+const currentEls = [
+  document.querySelector('#current--0'),
+  document.querySelector('#current--1'),
+];
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+scoreEls[0].textContent = 0;
+scoreEls[1].textContent = 0;
+diceEl.classList.add('hidden');
+
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+/** 주사위를 던져 주사위 이미지를 출력하는 함수 */
+const rollDice = () => {
+  // 1. Generating a random dice roll
+  let dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice === 1) {
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    players[1].classList.add('player--active');
+    players[0].classList.remove('player--active');
+  } else {
+    // Add dice to current score
+    currentScore += dice;
+    document.querySelector(`current-${activePlayer}`).textContent =
+      currentScore;
+    currentEls[0].textContent = currentScore; // change later
+  }
+};
+
+// Rolling dice
+btnRoll.addEventListener('click', rollDice);
